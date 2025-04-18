483920b6a711e24c3592523ab04723332ea1b4b4
Kailash Gautham <kailash.gautham@gmail.com>
2025-04-18 20:31:55 +0800

    Fix errors in scanner class

---
+++ CMakeFiles/interpreter.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/interpreter.dir/src/Scanner.cpp.o"
+  "CMakeFiles/interpreter.dir/src/Scanner.cpp.o.d"
+  "CMakeFiles/interpreter.dir/src/main.cpp.o"
+  "CMakeFiles/interpreter.dir/src/main.cpp.o.d"
+  "interpreter"
+  "interpreter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/interpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Scanner.cpp
@@ -0,0 +1,80 @@
+#include <string>
+#include <utility>
+#include <iostream>
+
+class Scanner {
+public:
+    explicit Scanner(std::string file_contents)
+        : file_contents(std::move(file_contents)), line_number(1), is_parsing_error(false) { }
+
+    [[nodiscard]] bool get_is_parsing_error() const {
+        return this->is_parsing_error;
+    }
+
+    void interpret_file_contents() {
+        if (!this->file_contents.empty()) {
+            for (int i = 0; i < file_contents.size(); i++) {
+                interpret_character(i);
+            }
+        }
+        std::cout << "EOF  null" << std::endl;
+    }
+
+private:
+    std::string file_contents;
+    int line_number;
+    bool is_parsing_error;
+
+    bool match_next_same(int& i, const char& c) const {
+        if (i + 1 == file_contents.size() || file_contents[i + 1] != c) {
+            return false;
+        }
+        i++;
+        return true;
+    }
+
+    void interpret_character(int& i) {
+        switch (this->file_contents[i]) {
+            case '(':
+                std::cout << "LEFT_PAREN ( null" << std::endl;
+                break;
+            case ')':
+                std::cout << "RIGHT_PAREN ) null" << std::endl;
+                break;
+            case '{':
+                std::cout << "LEFT_BRACE { null" << std::endl;
+                break;
+            case '}':
+                std::cout << "RIGHT_BRACE } null" << std::endl;
+                break;
+            case '*':
+                std::cout << "STAR * null" << std::endl;
+                break;
+            case '.':
+                std::cout << "DOT . null" << std::endl;
+                break;
+            case ',':
+                std::cout << "COMMA , null" << std::endl;
+                break;
+            case ';':
+                std::cout << "SEMICOLON ; null" << std::endl;
+                break;
+            case '+':
+                std::cout << "PLUS + null" << std::endl;
+                break;
+            case '-':
+                std::cout << "MINUS - null" << std::endl;
+                break;
+            case '=':
+                if (match_next_same(i, '=')) {
+                    std::cout << "EQUAL EQUAL == null" << std::endl;
+                } else {
+                    std::cout << "EQUAL = null" << std::endl;
+                }
+                break;
+            default:
+                fprintf(stderr, "[line %d] Error: Unexpected character: %c\n", line_number, this->file_contents[i]);
+                is_parsing_error = true;
+        }
+    }
+};
