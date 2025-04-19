3b3059aef5c7dc81e0995688dc41228ae50f37f7
Kailash Gautham <kailash.gautham@gmail.com>
2025-04-19 22:32:36 +0800

    Implement group parsing with stack

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue>
+#include <stack>
 #include <string>
 
 class Parser {
@@ -24,36 +26,60 @@ private:
     int line_number;
     int char_number;
     bool is_parsing_error;
-
+    std::stack<std::string> groups;
     void process_expr() {
         if (std::isdigit(this->file_contents[this->char_number])) {
-            process_number_literal();
-        }
-
-        else if (std::isalpha(this->file_contents[this->char_number])) {
-            std::cout << process_keyword() << std::endl;
-        }
-
-        else if (this->file_contents[this->char_number] == '"') {
+            const std::string number_literal = process_number_literal();
+            if (!groups.empty()) {
+                groups.top() += " " + number_literal;
+            } else {
+                std::cout << number_literal << std::endl;
+            }
+        } else if (std::isalpha(this->file_contents[this->char_number])) {
+            const std::string expr = process_word(true);
+            if (!groups.empty()) {
+                groups.top() += " " + expr;
+            } else {
+                std::cout << expr << std::endl;
+            }
+        } else if (this->file_contents[this->char_number] == '"') {
             this->char_number++;
-            process_string_literal();
+            const std::string expr = process_string_literal();
+            if (!groups.empty()) {
+                groups.top() += " " + expr;
+            } else {
+                std::cout << expr << std::endl;
+            }
+        } else if (this->file_contents[this->char_number] == '(') {
+            std::string new_group = "(group";
+            this->groups.push(std::move(new_group));
+        }
+        if (this->file_contents[this->char_number] == ')') {
+            groups.top() += ')';
+            const std::string top_group = groups.top();
+            groups.pop();
+            if (!groups.empty()) {
+                groups.top() += " " + top_group;
+            } else {
+                std::cout << top_group << std::endl;
+            }
         }
     }
 
-    void process_string_literal() {
+    std::string process_string_literal() {
         std::string expr;
         while (this->char_number < file_contents.size() && this->file_contents[this->char_number] != '"') {
-            expr += this->process_keyword();
+            expr += this->process_word(false);
             if (this->char_number < file_contents.size() && this->file_contents[this->char_number] != '"') {
                 expr += this->file_contents[this->char_number];
                 this->char_number++;
             }
         }
-        std::cout << expr << std::endl;
+        return expr;
     }
 
 
-    void process_number_literal() {
+    std::string process_number_literal() {
         std::string number_literal;
         bool decimal_found = false;
         int decimal_places = 0;
@@ -80,13 +106,16 @@ private:
         if (!decimal_found) {
             number_literal += ".0";
         }
-        std::cout << number_literal << std::endl;
+        return number_literal;
     }
 
-    std::string process_keyword() {
+    std::string process_word(const bool is_keyword) {
         std::string expr;
         while (this->char_number < file_contents.size()) {
-            if (std::isspace(this->file_contents[this->char_number]) || this->file_contents[this->char_number] == '"') {
+            if (
+                (is_keyword && !std::isalpha(this->file_contents[this->char_number])) ||
+                (!is_keyword && this->file_contents[this->char_number] == '"')
+            ) {
                 break;
             }
             expr += this->file_contents[this->char_number];
